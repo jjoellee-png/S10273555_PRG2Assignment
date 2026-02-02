94fe3def491450332bf7ddac6e60800aae4f01b9
Ohiokusaimaz <s10275515@connect.np.edu.sg>
2026-02-03 00:30:50 +0800

    Added classes Restaurant, OrderedFoodItem, Order, Customer

---
+++ OrderedFoodItem.cs
@@ -0,0 +1,35 @@
+﻿using S10273555_PRG2Assignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2Assignment
+{
+    public class OrderedFoodItem : FoodItem
+    {
+        public int QtyOrdered { get; set; }
+        public double SubTotal { get; set; }
+
+        // Correct constructor
+        public OrderedFoodItem(string itemName, string itemDesc, double itemPrice, string customise, int qty)
+            : base(itemName, itemDesc, itemPrice, customise) // pass actual values to FoodItem
+        {
+            QtyOrdered = qty;
+            SubTotal = QtyOrdered * ItemPrice; // calculate subtotal immediately
+        }
+
+        public double CalculateSubtotal()
+        {
+            SubTotal = QtyOrdered * ItemPrice;
+            return SubTotal;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " x " + QtyOrdered + " = $" + SubTotal.ToString("0.00");
+        }
+    }
+}
---
+++ Restaurant.cs
@@ -0,0 +1,56 @@
+﻿using S10273555_PRG2Assignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2Assignment
+{
+    class Restaurant 
+    {
+        public string RestaurantId { get; set; }
+        public string RestaurantName { get; set; }
+        public string RestaurantEmail { get; set; }
+
+        private List<Menu> menu = new List<Menu>();
+        private List<SpecialOffer> offers = new List<SpecialOffer>();
+        public Restaurant()
+        {
+
+        }
+        public Restaurant(string rId, string rName, string rEmail)
+        {
+            RestaurantId = rId;
+            RestaurantName = rName;
+            RestaurantEmail = rEmail;
+        }
+
+        public void DisplayOrders()
+        {
+
+        }
+        public void DisplaySpecialOffers()
+        {
+            foreach (var offer in offers)
+                Console.WriteLine(offer.ToString());
+        }
+        public void DisplayMenu()
+        {
+            foreach (var menu in menu)
+                menu.DisplayFoodItems(); //from menu 
+        }
+        public void AddMenu(Menu menuItem)
+        {
+            menu.Add(menuItem);
+        }
+        public bool RemoveMenu(Menu menuItem)
+        {
+            return menu.Remove(menuItem);
+        }
+        public override string ToString()
+        {
+            return $"{RestaurantName} ({RestaurantId}) - {RestaurantEmail}";
+        }
+    }
+}
